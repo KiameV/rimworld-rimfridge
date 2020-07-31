ab6482c48f8f9c7f6b8dd317a9311b0a5140905c
Karim Batbouta <kbatbouta66@gmail.com>
2020-07-31 14:41:48 -0700

    Forgot to add it in the previous commit.

---
+++ Source/FridgeCache.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace RimFridge
+{
+    [StaticConstructorOnStartup]
+    public static class FridgeCache
+    {
+        public static Dictionary<IntVec3, CompRefrigerator>[] FridgeGrid = new Dictionary<IntVec3, CompRefrigerator>[10];
+
+        static FridgeCache()
+        {
+            for (int i = 0; i < 10; i++) FridgeGrid[i] = new Dictionary<IntVec3, CompRefrigerator>(200);
+        }
+    }
+
+
+    [HarmonyPatch(typeof(Map), nameof(Map.FinalizeInit))]
+    static class Patch_ClearCache
+    {
+        static void Postfix(Map __instance)
+        {
+            FridgeCache.FridgeGrid[__instance.Index].Clear();
+        }
+    }
+}
